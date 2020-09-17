d719a84c2b92ab8c646256956c36ce015948bcdf
sbetsabe <bet.rlopez@gmail.com>
2020-09-17 00:00:42 -0600

    hola mundo

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Importamos nuestro componente
+import Hola from './Hola';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <div className="App-intro">
+          {/*Incluimos nuestro componente*/}
+          <Hola/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
